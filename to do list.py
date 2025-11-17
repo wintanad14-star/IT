3e7b4ae74b50ed94be1049012845bf855cfe1958
Wintana-star <wintana.d14@gmail.com>
2025-11-17 11:57:58 +0300

    Fix input handling in todo list functionality
    Refactor input handling for todo list operations.

--- to do list.py
+++ to do list.py
@@ -1,14 +1,14 @@
 todo_list=[]
 print("welcome!!!")
 print("press 1, if u want to see your todo list,press 2, if u want to add into your todo list and press 3, if u want to remove something to from your todo list")
-input=input()
-if input=="1":
+iun=input()
+if iun=="1":
     print("your todo list is",todo_list)
-elif input=="2":
+elif iun=="2":
     response=input("please add what you want to add to your todo list")
     todo_list.append(response)
     print("your new list is:", todo_list)
-elif input=="3":
+elif iun=="3":
     response2=input("what do you want to remove")
     todo_list.remove(response2)
     print("your new list is:",todo_list)
