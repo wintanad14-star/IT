193cdca696219f8e08eca28d00322bfdf210a91f
Wintana-star <wintana.d14@gmail.com>
2026-02-13 10:07:42 +0300

    Add files via upload

--- to do list.py
+++ to do list.py
@@ -1,19 +1,70 @@
-todo_list=[]
-print("welcome!!!")
-print("press 1, if u want to see your todo list,press 2, if u want to add into your todo list and press 3, if u want to remove something to from your todo list")
-iun=input()
-if iun=="1":
-    print("your todo list is",todo_list)
-elif iun=="2":
-    response=input("please add what you want to add to your todo list")
-    todo_list.append(response)
-    print("your new list is:", todo_list)
-elif iun=="3":
-    response2=input("what do you want to remove")
-    todo_list.remove(response2)
-    print("your new list is:",todo_list)
-else:
-    print("error, please try again")
+todo_list=[] 
+complete=[]
+
+def main_display():
+    for x in todo_list:
+        print(x)
+def addd():
+    z=input("enter your new task")
+    main_display()
+    todo_list.append(z)
+    
+def rem():
+    u=input("what do you want to remove")
+    main_display()
+    todo_list.remove(u)
+    print(u,"is removed from your todo list")
+def ed():
+    main_display()
+    o= input("Enter the task you want to edit: ")
+    if o in todo_list:
+        n= input("Enter the edited task: ")
+        i = todo_list.index(o)
+        todo_list[i] = n
+        print("Task updated!")
+    else:
+        print("Task not found!")
+        
+def mac():
+    main_display()
+    w=input("enter the task you want it to be marked as complete")
+    todo_list.remove(w)
+    complete.append(w)
+def mac_done():
+    print(complete)
+    
+def comp_display():
+      for w in complete:
+          print(w)
+              
+def interface():
+    while True:
+        print("=== TO-DO LIST ===")
+        main_display()
+        print("=== COMPLETED ===")
+        comp_display()
+        print("=== MAIN MENU ===")
+        print("1. ADD NEW TASK")
+        print("2. REMOVE TASK")
+        print("3. EDIT TASK")
+        print("4. MARK TASK AS COMPLETE")
+        print("5. EXIT")
+        c=int(input("enter your choice 1-5"))
+        if c==1:
+            addd()
+        elif c==2:
+            rem()
+        elif c==3:
+            ed()
+        elif c==4:
+            mac()
+           
+        elif c==5:
+            print("exitting, until next time 🫡🫡🫡🫡")
+            break
+interface()
+    
+    
+    
     
 
-    
